d812e4c918444f1ee28b1b00b562fd91c6c48d02
darivadeneira <generalmayorxd@gmail.com>
2025-02-20 16:29:41 -0500

    Add process memory usage tracking to system resources

--- app/services/system_service.py
+++ app/services/system_service.py
@@ -3,19 +3,51 @@ import psutil
 def get_system_resources():
     cpu = psutil.cpu_percent(interval=1)
     
+    process_act = psutil.process_iter()
+    process_list = []
+    for proc in psutil.process_iter(['pid', 'name', 'username']):
+        try:
+            proc_info = proc.info
+            process_list.append({
+                'pid': proc_info['pid'],
+                'name': proc_info['name'],
+                'user': proc_info['username']
+            })
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+
+
     memory_info = psutil.virtual_memory()
-    memory_used = memory_info.used / (1024 ** 2)  # Convertir a MB
-    memory_total = memory_info.total / (1024 ** 2)  # Convertir a MB
-    memory_percentage = memory_info.percent  # Uso en porcentaje
+    memory_used = memory_info.used / (1024 ** 2)  
+    memory_total = memory_info.total / (1024 ** 2)  
+    memory_percentage = memory_info.percent  
+
+    processes = []
     
-    disk_used = psutil.disk_usage('/').used / (1024 ** 3)  # Convertir a GB
+    for proc in psutil.process_iter(attrs=['pid', 'name', 'memory_info']):
+        try:
+            mem_info = proc.info['memory_info']
+            processes.append({
+                'pid': proc.info['pid'],
+                'name': proc.info['name'],
+                'memory_mb': round(mem_info.rss / (1024 * 1024), 2) # Convertir a MB
+            })
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            continue
+    
+    processes = sorted(processes, key=lambda p: p['memory_mb'], reverse=True)
+    
+    disk_used = psutil.disk_usage('/').used / (1024 ** 3)  
     
     return {
         'cpu': cpu,
+        'process_act': len(list(process_act)),
+        'processes': process_list,  # Cambiamos cpu_process por processes
         'memory': {
             'used': round(memory_used, 2),
             'total': round(memory_total, 2),
-            'percentage': memory_percentage
+            'percentage': memory_percentage,
         },
-        'disk': round(disk_used, 2)
+        'memory_processes': processes,
+        'disk': round(disk_used, 2),
     }
